e628dda22ba931ac2cd2fc9e21082aa01a88c8ec
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2012-06-08 03:00:14 +0000

    add every statement instrumenter

---
+++ trunk/configuration-detector/main/edu/washington/cs/conf/instrument/EveryStmtInstrumenter.java
@@ -0,0 +1,95 @@
+package edu.washington.cs.conf.instrument;
+
+import instrument.Globals;
+
+import java.io.Writer;
+
+import com.ibm.wala.shrikeBT.ConstantInstruction;
+import com.ibm.wala.shrikeBT.Constants;
+import com.ibm.wala.shrikeBT.Disassembler;
+import com.ibm.wala.shrikeBT.IInstruction;
+import com.ibm.wala.shrikeBT.Instruction;
+import com.ibm.wala.shrikeBT.MethodData;
+import com.ibm.wala.shrikeBT.MethodEditor;
+import com.ibm.wala.shrikeBT.Util;
+import com.ibm.wala.shrikeBT.analysis.Verifier;
+import com.ibm.wala.shrikeBT.shrikeCT.ClassInstrumenter;
+import com.ibm.wala.shrikeCT.ClassReader;
+import com.ibm.wala.shrikeCT.ClassWriter;
+
+public class EveryStmtInstrumenter extends AbstractInstrumenter {
+	
+	static final String fieldName = "_Stmt_enable_trace";
+	
+	static final Instruction getTracer = Util.makeGet(StmtTracer.class, "tracer");
+	static final Instruction callTrace = Util.makeInvoke(StmtTracer.class, "trace", new Class[] { String.class });
+	
+	public EveryStmtInstrumenter() {
+		//empty on purpose
+	}
+
+	@Override
+	protected void doClass(ClassInstrumenter ci, Writer w) throws Exception {
+		final String className = ci.getReader().getName();
+	    w.write("Class: " + className + "\n");
+	    w.flush();
+
+	    for (int m = 0; m < ci.getReader().getMethodCount(); m++) {
+	      MethodData d = ci.visitMethod(m);
+	      // d could be null, e.g., if the method is abstract or native
+	      if (d != null) {
+	        w.write("Instrumenting " + ci.getReader().getMethodName(m) + " " + ci.getReader().getMethodType(m) + ":\n");
+	        w.flush();
+	        if (disasm) {
+	          w.write("Initial ShrikeBT code:\n");
+	          (new Disassembler(d)).disassembleTo(w);
+	          w.flush();
+	        }
+	        if (verify) {
+	          Verifier v = new Verifier(d);
+	          v.verify();
+	        }
+
+	        MethodEditor me = new MethodEditor(d);
+	        me.beginPass();
+
+	        //the unique method signature
+        	String methodSig = this.getMethodSignature(d);
+        	
+        	//instrument every instruction
+	        int length = me.getInstructions().length;
+	        for(int i = 0; i < length; i++) {
+	        	IInstruction inst = me.getInstructions()[i];
+	        	final String msg = methodSig + SEP + inst.toString() +  SEP  + i;  //a unique encoding
+	        	me.insertBefore(i, new MethodEditor.Patch() {
+                    @Override
+                    public void emitTo(MethodEditor.Output w) {
+                      //w.emit(getSysErr);
+                  	  w.emit(getTracer);
+                      w.emit(ConstantInstruction.makeString(msg));
+                      w.emit(callTrace);
+                      //keep the statistics
+                      InstrumentStats.addInsertedInstructions(1);
+                    }
+                 });
+	        }
+	        
+	        // this updates the data, instrumentation ends
+	        me.applyPatches();
+	        if (disasm) {
+	          w.write("Final ShrikeBT code:\n");
+	          (new Disassembler(d)).disassembleTo(w);
+	          w.write(Globals.lineSep);
+	          w.flush();
+	        }
+	      }
+	    }
+
+	    if (ci.isChanged()) {
+	      ClassWriter cw = ci.emitClass();
+	      cw.addField(ClassReader.ACC_PUBLIC | ClassReader.ACC_STATIC, fieldName, Constants.TYPE_boolean, new ClassWriter.Element[0]);
+	      instrumenter.outputModifiedClass(ci, cw);
+	    }
+	}
+
+}
