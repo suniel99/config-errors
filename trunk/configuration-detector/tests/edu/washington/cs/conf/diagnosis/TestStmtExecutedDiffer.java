db94233878a4e6cc371507faf6d1abab02d9bddb
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2012-06-08 19:49:12 +0000

    add test cases for baseline diagnosers

--- trunk/configuration-detector/tests/edu/washington/cs/conf/diagnosis/TestStmtExecutedDiffer.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/diagnosis/TestStmtExecutedDiffer.java
@@ -25,6 +25,42 @@ public class TestStmtExecutedDiffer extends TestCase {
             = new LinkedList<Collection<StmtExecuted>>();
 		badRuns.add(bad1);
 		
+		//do diff
+		computeScore(goodRuns, badRuns);
+	}
+	
+	public void testDiffer2() {
+		String dir = "./tests/edu/washington/cs/conf/diagnosis/";
+		List<StmtExecuted> good1 = StmtFileReader.readStmts(dir + "test.baseline-good1-no-args.txt");
+		List<StmtExecuted> good2 = StmtFileReader.readStmts(dir + "test.baseline-good2-option1-1.txt");
+		List<StmtExecuted> good3 = StmtFileReader.readStmts(dir + "test.baseline-good3-option1-1-option2-2.txt");
+		List<StmtExecuted> good4 = StmtFileReader.readStmts(dir + "test.baseline-good4-option1-1-option2-2-option3-3.txt");
+		List<StmtExecuted> good5 = StmtFileReader.readStmts(dir + "test.baseline-good5-option1-1-option2-2-option3--3.txt");
+		List<StmtExecuted> good6 = StmtFileReader.readStmts(dir + "test.baseline-good6-option1-1-option2--2-option3-3.txt");
+		
+		List<StmtExecuted> bad1 = StmtFileReader.readStmts(dir + "test.baseline-bad-1-2-0.txt");
+		
+		StmtExecuted.addSourceNumber(dir + "test.baseline.diagnoser.jar", good1,good2, good3, good4, good5, good4, bad1);
+		
+		Collection<Collection<StmtExecuted>> goodRuns
+	        = new LinkedList<Collection<StmtExecuted>>();
+		goodRuns.add(good1);
+		goodRuns.add(good2);
+		goodRuns.add(good3);
+		goodRuns.add(good4);
+		goodRuns.add(good5);
+		goodRuns.add(good6);
+		
+		Collection<Collection<StmtExecuted>> badRuns
+            = new LinkedList<Collection<StmtExecuted>>();
+		badRuns.add(bad1);
+		
+		//do diff
+		computeScore(goodRuns, badRuns);
+	}
+	
+	static Map<String, Float> computeScore(Collection<Collection<StmtExecuted>> goodRuns,
+			Collection<Collection<StmtExecuted>> badRuns) {
 		StmtExecutedDiffer differ = new StmtExecutedDiffer(goodRuns, badRuns);
 		differ.computeStmtScores();
 		Map<String, Float> scores = differ.getRankedStmts();
@@ -33,6 +69,7 @@ public class TestStmtExecutedDiffer extends TestCase {
 			System.out.println(s + "   " + scores.get(s));
 		}
 		
+		return scores;
 	}
 
 }
