ac1929828613371101f518f418a9ceb9322a1805
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2012-06-09 04:31:25 +0000

    remove class locations

---
+++ trunk/configuration-detector/main/edu/washington/cs/conf/analysis/ConfEntityRepository.java
@@ -0,0 +1,27 @@
+package edu.washington.cs.conf.analysis;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
+
+public class ConfEntityRepository {
+
+	public final Collection<ConfEntity> entities = new LinkedHashSet<ConfEntity>();
+	
+	public ConfEntityRepository(Collection<ConfEntity> entities) {
+		this.entities.addAll(entities);
+		if(this.entities.size() != entities.size()) {
+			System.err.println("Warning, size not equal. Given: "
+					+ entities.size() + ", but result in: " + this.entities.size());
+		}
+	}
+	
+	public ConfEntity lookupConfEntity(String fullName) {
+		for(ConfEntity entity : entities) {
+			if(entity.getFullConfName().equals(fullName)) {
+				return entity;
+			}
+		}
+		return null;
+	}
+}
