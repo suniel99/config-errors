0635f255b93243b37a7f987967b3140670cdd206
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2012-06-13 00:44:18 +0000

    some major changes: (1) change the instrumenter (add more info, such as line number, predicate txt), also change the parsing semantic (2) adding methodsig + instruction_index as a unified point, (3) extract source line predicate text from files

--- trunk/configuration-detector/tests/edu/washington/cs/conf/experiments/randoop/TestInstrumentRandoop.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/experiments/randoop/TestInstrumentRandoop.java
@@ -1,12 +1,16 @@
-package edu.washington.cs.conf.instrument;
+package edu.washington.cs.conf.experiments.randoop;
 
 import edu.washington.cs.conf.analysis.ConfOutputSerializer;
+import edu.washington.cs.conf.instrument.ConfInstrumenter;
+import edu.washington.cs.conf.instrument.InstrumentSchema;
+import edu.washington.cs.conf.instrument.InstrumentStats;
 import junit.framework.TestCase;
 
 public class TestInstrumentRandoop extends TestCase {
 	
 	public void testInstrument() throws Exception {
 		String filePath = "./randoop_option_instr_ser.dat";
+		String randoopSrcDir = "./subjects/randoop/randoop-src/"; //NOT used yet
 		InstrumentSchema schema = ConfOutputSerializer.deserializeAsSchema(filePath);
 		
 		ConfInstrumenter instrumenter = new ConfInstrumenter(schema);
---
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/experiments/randoop/TestRandoopBaseline.java
@@ -0,0 +1,15 @@
+package edu.washington.cs.conf.experiments.randoop;
+
+import junit.framework.TestCase;
+
+public class TestRandoopBaseline extends TestCase {
+	
+	public void testExecutedLine() {
+		
+	}
+	
+	public void testAffectedMethods() {
+		
+	}
+
+}
