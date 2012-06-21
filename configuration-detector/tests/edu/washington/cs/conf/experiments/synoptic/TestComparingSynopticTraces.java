e561e1fffb510e2792cde354c84b39393fc441f3
racezhang <racezhang@gmail.com>
2012-06-21 18:12:19 +0000

    add the synoptic data and test cases

--- configuration-detector/tests/edu/washington/cs/conf/experiments/synoptic/TestComparingSynopticTraces.java
+++ configuration-detector/tests/edu/washington/cs/conf/experiments/synoptic/TestComparingSynopticTraces.java
@@ -1,7 +1,16 @@
 package edu.washington.cs.conf.experiments.synoptic;
 
+import edu.washington.cs.conf.diagnosis.ProfileDistanceCalculator.DistanceType;
+import edu.washington.cs.conf.experiments.CommonUtils;
 import junit.framework.TestCase;
 
 public class TestComparingSynopticTraces extends TestCase {
 
+	public void test1() {
+	    String goodRunTrace = "./experiments/synoptic-database/2pc_3nodes_100tx_bad-injected.txt";
+	    String badRunTrace = "./experiments/synoptic-database/2pc_3nodes_100tx_good.txt";
+//	    CommonUtils.compareTraceDistance(goodRunTrace, badRunTrace, DistanceType.SUBTRACTION, 0.0f);
+	    CommonUtils.compareTraceDistance(goodRunTrace, badRunTrace, DistanceType.INTERPRODUCT, 0.2584772f);
+	}
+	
 }
--- configuration-detector/tests/edu/washington/cs/conf/experiments/synoptic/TestDiagnoseSynopticOptions.java
+++ configuration-detector/tests/edu/washington/cs/conf/experiments/synoptic/TestDiagnoseSynopticOptions.java
@@ -1,7 +1,22 @@
 package edu.washington.cs.conf.experiments.synoptic;
 
+import java.util.List;
+
+import edu.washington.cs.conf.analysis.ConfEntity;
+import edu.washington.cs.conf.analysis.ConfEntityRepository;
+import edu.washington.cs.conf.diagnosis.PredicateProfileBasedDiagnoser.RankType;
+import edu.washington.cs.conf.experiments.CommonUtils;
+import edu.washington.cs.conf.experiments.SynopticExpUtils;
 import junit.framework.TestCase;
 
 public class TestDiagnoseSynopticOptions extends TestCase {
-
+	
+	public void test1() {
+		String goodRunTrace = "./experiments/synoptic-database/2pc_3nodes_100tx_bad-injected.txt";
+	    String badRunTrace = "./experiments/synoptic-database/2pc_3nodes_100tx_good.txt";
+	    List<ConfEntity> synopticConfList = SynopticExpUtils.getSynopticList();
+		ConfEntityRepository repo = new ConfEntityRepository(synopticConfList);
+	    CommonUtils.diagnoseOptions(goodRunTrace, badRunTrace, true, repo, RankType.SINGLE_IMPORT, 0.3f);
+	}
+	
 }
