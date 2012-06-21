0598f5f5f09f7c19ed76dd4f28b842c617397f0e
racezhang <racezhang@gmail.com>
2012-06-21 18:34:06 +0000

    add soot example of keeping line numbers

--- configuration-detector/tests/edu/washington/cs/conf/experiments/soot/TestComparingSootTraces.java
+++ configuration-detector/tests/edu/washington/cs/conf/experiments/soot/TestComparingSootTraces.java
@@ -1,7 +1,16 @@
 package edu.washington.cs.conf.experiments.soot;
 
+import edu.washington.cs.conf.diagnosis.ProfileDistanceCalculator.DistanceType;
+import edu.washington.cs.conf.experiments.CommonUtils;
 import junit.framework.TestCase;
 
 public class TestComparingSootTraces extends TestCase {
 
+	public void test1() {
+	    String goodRunTrace = "./experiments/soot-database/soot_helloworld_with_keepline.txt";
+	    String badRunTrace = "./experiments/soot-database/soot_helloworld_no_keepline.txt";
+//	    CommonUtils.compareTraceDistance(goodRunTrace, badRunTrace, DistanceType.SUBTRACTION, 0.0f);
+	    CommonUtils.compareTraceDistance(goodRunTrace, badRunTrace, DistanceType.INTERPRODUCT, 0.10196239f);
+	}
+	
 }
--- configuration-detector/tests/edu/washington/cs/conf/experiments/soot/TestDiagnoseSootOptions.java
+++ configuration-detector/tests/edu/washington/cs/conf/experiments/soot/TestDiagnoseSootOptions.java
@@ -1,7 +1,22 @@
 package edu.washington.cs.conf.experiments.soot;
 
+import java.util.List;
+
+import edu.washington.cs.conf.analysis.ConfEntity;
+import edu.washington.cs.conf.analysis.ConfEntityRepository;
+import edu.washington.cs.conf.diagnosis.PredicateProfileBasedDiagnoser.RankType;
+import edu.washington.cs.conf.experiments.CommonUtils;
+import edu.washington.cs.conf.experiments.SootExpUtils;
 import junit.framework.TestCase;
 
 public class TestDiagnoseSootOptions extends TestCase {
-
+	
+	public void test1() {
+		String goodRunTrace = "./experiments/soot-database/soot_helloworld_with_keepline.txt";
+	    String badRunTrace = "./experiments/soot-database/soot_helloworld_no_keepline.txt";
+	    List<ConfEntity> sootConfList = SootExpUtils.getSootConfList();
+		ConfEntityRepository repo = new ConfEntityRepository(sootConfList);
+	    CommonUtils.diagnoseOptions(goodRunTrace, badRunTrace, false, repo, RankType.SINGLE_IMPORT, 0.2f);
+	}
+	
 }
