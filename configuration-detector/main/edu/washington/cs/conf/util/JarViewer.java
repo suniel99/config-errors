f0d7cacb33f56eca87d2e297ba30b0add61a67fb
racezhang <racezhang@gmail.com>
2012-06-29 00:53:50 +0000

    add the jar viewer code

---
+++ configuration-detector/main/edu/washington/cs/conf/util/JarViewer.java
@@ -0,0 +1,48 @@
+package edu.washington.cs.conf.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.LinkedHashSet;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+public class JarViewer {
+
+	public static Collection<ZipEntry> getContents(String jarFilePath)
+			throws ZipException, IOException {
+		return getContents(new File(jarFilePath));
+	}
+
+	public static Collection<ZipEntry> getContents(File f) throws ZipException,
+			IOException {
+		Collection<ZipEntry> entries = new LinkedHashSet<ZipEntry>();
+		ZipFile jarFile = new ZipFile(f);
+		Enumeration<? extends ZipEntry> e = jarFile.entries();
+		while (e.hasMoreElements()) {
+			ZipEntry ze = e.nextElement();
+			entries.add(ze);
+		}
+		return entries;
+	}
+
+	public static Collection<String> getContentsAsStr(File f)
+			throws ZipException, IOException {
+		Collection<ZipEntry> entries = getContents(f);
+		Collection<String> strs = new LinkedHashSet<String>();
+		for (ZipEntry e : entries) {
+			strs.add(e.toString());
+		}
+		return strs;
+	}
+
+	public static void main(String[] args) throws ZipException, IOException {
+		Collection<String> content = getContentsAsStr(new File(args[0]));
+		for (String c : content) {
+			System.out.println(c);
+		}
+
+	}
+}
