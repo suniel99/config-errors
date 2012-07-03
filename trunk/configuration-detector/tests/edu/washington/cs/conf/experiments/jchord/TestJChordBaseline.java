efbd6ee3120daa117814dee206255927ea19228f
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2012-07-03 02:04:45 +0000

    analyze invariants in chord

--- trunk/configuration-detector/tests/edu/washington/cs/conf/experiments/jchord/TestJChordBaseline.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/experiments/jchord/TestJChordBaseline.java
@@ -2,9 +2,15 @@ package edu.washington.cs.conf.experiments.jchord;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+import edu.washington.cs.conf.analysis.ConfEntity;
 import edu.washington.cs.conf.analysis.ConfPropOutput;
+import edu.washington.cs.conf.diagnosis.MethodBasedDiagnoser;
 import edu.washington.cs.conf.diagnosis.StmtCoverageBasedDiagnoserMain;
+import edu.washington.cs.conf.experiments.RandoopExpUtils;
+import edu.washington.cs.conf.experiments.randoop.TestSliceRandoopConfigOptions;
 import edu.washington.cs.conf.experiments.soot.TestSliceSootConfigOptions;
 import edu.washington.cs.conf.instrument.EveryStmtInstrumenter;
 import edu.washington.cs.conf.instrument.InstrumentStats;
@@ -34,7 +40,27 @@ public class TestJChordBaseline extends TestCase {
 				"./experiments/jchord-baseline-stmt/good_dlog.txt"};
 		
 		StmtCoverageBasedDiagnoserMain.findResponsibleOptions(outputs, badStmtFiles, goodStmtFiles);
+	}
 	
+	public void testDiagnoseOptionsByInvariantAnalysis() throws Exception {
+		String goodInvFile1 = "D:\\research\\configurations\\workspace\\configuration-detector\\subjects\\jchord\\datarace.inv.gz";
+		String goodInvFile2 = "D:\\research\\configurations\\workspace\\configuration-detector\\subjects\\jchord\\deadlock.inv.gz";
+		String goodInvFile3 = "D:\\research\\configurations\\workspace\\configuration-detector\\subjects\\jchord\\dlog.inv.gz";
+		
+		String badInvFile = "D:\\research\\configurations\\workspace\\configuration-detector\\subjects\\jchord\\datarace-norace.inv.gz";
+		//Set<String> affectedMethods = getAffectedMethods(goodInvFile, badInvFile);
+		Collection<ConfPropOutput> confs = TestSliceJChordConfigOptions.getJChordConfOutputs();
+		
+		List<ConfEntity> entities = MethodBasedDiagnoser.computeResponsibleOptions(Arrays.asList(goodInvFile1, goodInvFile2, goodInvFile3), 
+				badInvFile, confs);
+		
+		System.out.println(entities.size());
+		int i = 0;
+		for(ConfEntity entity : entities) {
+			System.out.println((i+1) + ". " + entity);
+			i++;
+		}
+		
 	}
 	
 }
