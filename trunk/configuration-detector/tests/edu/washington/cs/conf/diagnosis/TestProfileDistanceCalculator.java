22579aa2b965020c8f6c1c383bc4e1622ecae18b
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2012-07-23 18:09:25 +0000

    code for computing cosine similarity

--- trunk/configuration-detector/tests/edu/washington/cs/conf/diagnosis/TestProfileDistanceCalculator.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/diagnosis/TestProfileDistanceCalculator.java
@@ -13,18 +13,18 @@ public class TestProfileDistanceCalculator extends TestCase {
 	
 	@Override
 	public void setUp() {
-		PredicateProfile p1 = new PredicateProfile("conf1", "context1", 100, 50);
-		PredicateProfile p2 = new PredicateProfile("conf2", "context2", 100, 60);
-		PredicateProfile p3 = new PredicateProfile("conf3", "context3", 100, 70);
-		PredicateProfile p4 = new PredicateProfile("conf4", "context4", 100, 80);
-		PredicateProfile p5 = new PredicateProfile("conf5", "context5", 100, 90);
+		PredicateProfile p1 = new PredicateProfile("conf1", "context1_index_1", 100, 50);
+		PredicateProfile p2 = new PredicateProfile("conf2", "context2_index_1", 100, 60);
+		PredicateProfile p3 = new PredicateProfile("conf3", "context3_index_1", 100, 70);
+		PredicateProfile p4 = new PredicateProfile("conf4", "context4_index_1", 100, 80);
+		PredicateProfile p5 = new PredicateProfile("conf5", "context5_index_1", 100, 90);
 		t1 = PredicateProfileTuple.createGoodRun("tuple1", Arrays.asList(p1, p2, p3, p4, p5));
 		
-		PredicateProfile p11 = new PredicateProfile("conf1", "context1", 100, 50);
-		PredicateProfile p21 = new PredicateProfile("conf21", "context2", 100, 90);
-		PredicateProfile p31 = new PredicateProfile("conf3", "context3", 100, 70);
-		PredicateProfile p41 = new PredicateProfile("conf4", "context4", 100, 50);
-		PredicateProfile p51 = new PredicateProfile("conf51", "context5", 100, 90);
+		PredicateProfile p11 = new PredicateProfile("conf1", "context1_index_1", 100, 50);
+		PredicateProfile p21 = new PredicateProfile("conf21", "context2_index_1", 100, 90);
+		PredicateProfile p31 = new PredicateProfile("conf3", "context3_index_1", 100, 70);
+		PredicateProfile p41 = new PredicateProfile("conf4", "context4_index_1", 100, 50);
+		PredicateProfile p51 = new PredicateProfile("conf51", "context5_index_1", 100, 90);
 		t2 = PredicateProfileTuple.createGoodRun("tuple2", Arrays.asList(p11, p21, p31, p41, p51));
 	}
 	
@@ -48,9 +48,76 @@ public class TestProfileDistanceCalculator extends TestCase {
 	public void test2() {
 		float interprodDistance = ProfileDistanceCalculator.computeDistance(t1, t2, DistanceType.INTERPRODUCT);
 		System.out.println("interproduction distance: " + interprodDistance);
+		assertEquals(0.65848213f, interprodDistance);
 		
 		float substractDistance = ProfileDistanceCalculator.computeDistance(t1, t2, DistanceType.SUBTRACTION);
 		System.out.println("substraction distance: " + substractDistance);
+		assertEquals(0.5142857f, substractDistance);
+		
+		float cosineDistance = ProfileDistanceCalculator.computeDistance(t1, t2, DistanceType.COSINE);
+		System.out.println("cosine distance: " + cosineDistance);
 	}
 	
+	public void testCosineDistance_Zero() {
+		PredicateProfile p1 = new PredicateProfile("conf1", "context1_index_1", 100, 50);
+		PredicateProfile p2 = new PredicateProfile("conf2", "context2_index_1", 100, 60);
+		PredicateProfile p3 = new PredicateProfile("conf3", "context3_index_1", 100, 70);
+		PredicateProfile p4 = new PredicateProfile("conf4", "context4_index_1", 100, 80);
+		PredicateProfile p5 = new PredicateProfile("conf5", "context5_index_1", 100, 90);
+		t1 = PredicateProfileTuple.createGoodRun("tuple1", Arrays.asList(p1, p2, p3, p4, p5));
+		
+		PredicateProfile p11 = new PredicateProfile("conf1", "context1_index_1", 100, 50);
+		PredicateProfile p21 = new PredicateProfile("conf2", "context2_index_1", 100, 60);
+		PredicateProfile p31 = new PredicateProfile("conf3", "context3_index_1", 100, 70);
+		PredicateProfile p41 = new PredicateProfile("conf4", "context4_index_1", 100, 80);
+		PredicateProfile p51 = new PredicateProfile("conf5", "context5_index_1", 100, 90);
+		t2 = PredicateProfileTuple.createGoodRun("tuple2", Arrays.asList(p11, p21, p31, p41, p51));
+		
+		float cosineDistance = ProfileDistanceCalculator.computeDistance(t1, t2, DistanceType.COSINE);
+		System.out.println("cosine distance: " + cosineDistance);
+		
+		assertTrue(cosineDistance < 0.00001f);
+	}
+	
+	public void testCosineDistance_One() {
+		PredicateProfile p1 = new PredicateProfile("conf1", "context1_index_1", 100, 0);
+		PredicateProfile p2 = new PredicateProfile("conf2", "context2_index_1", 100, 0);
+		PredicateProfile p3 = new PredicateProfile("conf3", "context3_index_1", 100, 0);
+		PredicateProfile p4 = new PredicateProfile("conf4", "context4_index_1", 100, 0);
+		PredicateProfile p5 = new PredicateProfile("conf5", "context5_index_1", 100, 0);
+		t1 = PredicateProfileTuple.createGoodRun("tuple1", Arrays.asList(p1, p2, p3, p4, p5));
+		
+		PredicateProfile p11 = new PredicateProfile("conf1", "context1_index_1", 100, 50);
+		PredicateProfile p21 = new PredicateProfile("conf2", "context2_index_1", 100, 60);
+		PredicateProfile p31 = new PredicateProfile("conf3", "context3_index_1", 100, 70);
+		PredicateProfile p41 = new PredicateProfile("conf4", "context4_index_1", 100, 80);
+		PredicateProfile p51 = new PredicateProfile("conf5", "context5_index_1", 100, 90);
+		t2 = PredicateProfileTuple.createGoodRun("tuple2", Arrays.asList(p11, p21, p31, p41, p51));
+		
+		float cosineDistance = ProfileDistanceCalculator.computeDistance(t1, t2, DistanceType.COSINE);
+		System.out.println("cosine distance: " + cosineDistance);
+		
+		assertTrue(cosineDistance == 1.0f);
+	}
+	
+	public void testCosineDistance_NOTEEXE() {
+		PredicateProfile p1 = new PredicateProfile("conf11", "context1_index_1", 100, 10);
+		PredicateProfile p2 = new PredicateProfile("conf21", "context2_index_1", 100, 10);
+		PredicateProfile p3 = new PredicateProfile("conf31", "context3_index_1", 100, 10);
+		PredicateProfile p4 = new PredicateProfile("conf41", "context4_index_1", 100, 10);
+		PredicateProfile p5 = new PredicateProfile("conf51", "context5_index_1", 100, 10);
+		t1 = PredicateProfileTuple.createGoodRun("tuple1", Arrays.asList(p1, p2, p3, p4, p5));
+		
+		PredicateProfile p11 = new PredicateProfile("conf1", "context1_index_1", 100, 50);
+		PredicateProfile p21 = new PredicateProfile("conf2", "context2_index_1", 100, 60);
+		PredicateProfile p31 = new PredicateProfile("conf3", "context3_index_1", 100, 70);
+		PredicateProfile p41 = new PredicateProfile("conf4", "context4_index_1", 100, 80);
+		PredicateProfile p51 = new PredicateProfile("conf5", "context5_index_1", 100, 90);
+		t2 = PredicateProfileTuple.createGoodRun("tuple2", Arrays.asList(p11, p21, p31, p41, p51));
+		
+		float cosineDistance = ProfileDistanceCalculator.computeDistance(t1, t2, DistanceType.COSINE);
+		System.out.println("cosine distance: " + cosineDistance);
+		
+		assertTrue(cosineDistance == 1.0f);
+	}
 }
