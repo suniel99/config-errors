2d5c9a4a8c2b8fa01ba178fa5cc061d05b65d6dd
racezhang <racezhang@gmail.com>
2013-04-09 23:43:20 +0000

    implement the merge jar function

--- configuration-detector/main/edu/washington/cs/conf/util/JarUtils.java
+++ configuration-detector/main/edu/washington/cs/conf/util/JarUtils.java
@@ -11,6 +11,75 @@ import java.util.zip.ZipFile;
 
 public class JarUtils {
 
+	public static final String ANT_BAT = "D:\\develop-tools\\apache-ant-1.8.2\\bin\\ant.bat";
+	
+	public static final String ANT_BUILD_FILE =
+		"D:\\research\\configurations\\workspace\\configuration-detector\\main\\edu\\washington\\cs\\conf\\util\\build.xml";
+	
+	public static final String TRACER_FILE =
+		"D:\\research\\configurations\\workspace\\configuration-detector\\evoltracer.jar";
+	
+	public static final String MERGED_JAR = "merged.jar";
+	
+	public static boolean DELETE_DIR = true;
+	
+	public static File mergeWithTracer(File originalFile,
+			File tracerFile, File mergedFile) {
+		System.out.println("Start to merge: " + originalFile.getAbsolutePath()
+				+ Globals.lineSep + ", with: " + tracerFile.getAbsolutePath()
+				+ Globals.lineSep + ", into: " + mergedFile.getAbsolutePath());
+		try {
+			//if existed, first remove it
+			if (mergedFile.exists()) {
+				mergedFile.delete();
+			} else {
+				//create the parent folder
+				if(!mergedFile.getParentFile().exists()) {
+					mergedFile.getParentFile().mkdirs();
+				}
+			}
+			mergedFile.createNewFile();
+			
+			//here is the steps to merge the original jar with the
+			//tracer jar:
+			//1. create a tmp dir
+			//2. copy the ant file, original file, and tracerFile there
+			//3. run ant in that dir
+			//4. copy the merged jar back to mergedFile
+			//5. delete all the temp dir
+			File tmpDir = Files.createTempDirectoryNoExp();
+			System.out.println("create tmp dir: " + tmpDir.getAbsolutePath());
+			
+			File antFile = new File(ANT_BUILD_FILE);
+			File destAntFile = new File(tmpDir.getAbsolutePath() + Globals.fileSep + antFile.getName());
+			Files.copyFileNoExp(antFile, destAntFile);
+
+			File destTracerFile = new File(tmpDir.getAbsolutePath() + Globals.fileSep + tracerFile.getName());
+			Files.copyFileNoExp(tracerFile, destTracerFile);
+			
+			File destJarFile = new File(tmpDir.getAbsolutePath() + Globals.fileSep + originalFile.getName());
+			Files.copyFileNoExp(originalFile, destJarFile);
+			
+			System.out.println("Running ant to merge jars...");
+			Command.exec(new String[]{ANT_BAT, "-f", destAntFile.getAbsolutePath()});
+			
+			System.out.println("Copying merged file...");
+			File mergedJarFile = new File(tmpDir.getAbsolutePath() + Globals.fileSep + MERGED_JAR);
+			Utils.checkFileExistence(mergedJarFile.getAbsolutePath());
+			Files.copyFileNoExp(mergedJarFile, mergedFile);
+			
+			if(DELETE_DIR) {
+			    System.out.println("Delete all tmp dir: " + tmpDir.getAbsolutePath());
+		 	    Files.deleteDirectory(tmpDir);
+			}
+			
+		} catch (Throwable e) {
+			throw new Error(e);
+		}
+		System.out.println("Merge completed!");
+		return mergedFile;
+	}
+	
 	public static Collection<ZipEntry> getContents(String jarFilePath)
 			throws ZipException, IOException {
 		return getContents(new File(jarFilePath));
@@ -39,10 +108,12 @@ public class JarUtils {
 	}
 
 	public static void main(String[] args) throws ZipException, IOException {
-		Collection<String> content = getContentsAsStr(new File(args[0]));
-		for (String c : content) {
-			System.out.println(c);
-		}
-
+//		Collection<String> content = getContentsAsStr(new File(args[0]));
+//		for (String c : content) {
+//			System.out.println(c);
+//		}
+		
+		Command.exec(new String[]{ANT_BAT, "-f", "D:\\research\\confevol\\subject-programs\\ant-test\\build.xml"});
+		//Command.runCommand(command, prompt, verbose, nonVerboseMessage, gobbleChars)
 	}
 }
