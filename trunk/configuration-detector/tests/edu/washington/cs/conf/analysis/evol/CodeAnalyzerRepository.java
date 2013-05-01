c441fba1ab87011054b41a83964eaeeffe4c2ea0
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2013-05-01 23:16:54 +0000

    update some tests

---
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/analysis/evol/CodeAnalyzerRepository.java
@@ -0,0 +1,34 @@
+package edu.washington.cs.conf.analysis.evol;
+
+import edu.washington.cs.conf.analysis.ConfigurationSlicer.CG;
+import edu.washington.cs.conf.util.Globals;
+
+public class CodeAnalyzerRepository {
+
+	public static CodeAnalyzer getRandoop121Analyzer() {
+		String randoop121Path = "D:\\research\\confevol\\subject-programs\\randoop\\randoop-1.2.1\\randoop-1.2.1.jar";
+		
+		String randoopMain = "Lrandoop/main/Main";
+		
+		CodeAnalyzer coder121 = new CodeAnalyzer(randoop121Path, randoopMain);
+		coder121.slicer.setExclusionFile("JavaAllExclusions.txt");
+		coder121.slicer.setCGType(CG.ZeroCFA);
+		
+		return coder121;
+	}
+	
+	public static CodeAnalyzer getRandoop132Analyzer() {
+		String randoop132Path = "D:\\research\\confevol\\subject-programs\\randoop\\randoop-1.3.2\\randoop-1.3.2.jar"
+			+ Globals.pathSep + "D:\\research\\confevol\\subject-programs\\randoop\\randoop-1.3.2\\lib\\plume.jar"
+			+ Globals.pathSep + "D:\\research\\confevol\\subject-programs\\randoop\\randoop-1.3.2\\lib\\jakarta-oro-2.0.8.jar";
+		
+		String randoopMain = "Lrandoop/main/Main";
+		
+		CodeAnalyzer coder132 = new CodeAnalyzer(randoop132Path, randoopMain);
+		coder132.slicer.setExclusionFile("JavaAllExclusions.txt");
+		coder132.slicer.setCGType(CG.ZeroCFA);
+		
+		return coder132;
+	}
+	
+}
