fd9daeb07336c30fafc3c245c3b0e2828a832957
racezhang <racezhang@gmail.com>
2013-06-05 21:21:44 +0000

    add the analysis scope

--- configuration-detector/main/edu/washington/cs/conf/analysis/evol/MethodMatchingLogics.java
+++ configuration-detector/main/edu/washington/cs/conf/analysis/evol/MethodMatchingLogics.java
@@ -11,6 +11,8 @@ import edu.washington.cs.conf.util.WALAUtils;
 //the strategy taken to match methods
 public class MethodMatchingLogics {
 	
+	public static boolean USE_FUZZING_MATCHING = true;
+	
 	public final CodeAnalyzer oldAnalyzer;
 	public final CodeAnalyzer newAnalyzer;
 	
@@ -18,13 +20,16 @@ public class MethodMatchingLogics {
 	private int lookahead = 5;
 	
 	public final MethodMatcher matcher;
+	public final AnalysisScope scope;
 	
-	public MethodMatchingLogics(CodeAnalyzer oldAnalyzer, CodeAnalyzer newAnalyzer) {
+	public MethodMatchingLogics(CodeAnalyzer oldAnalyzer, CodeAnalyzer newAnalyzer, AnalysisScope scope) {
 		Utils.checkNotNull(oldAnalyzer);
 		Utils.checkNotNull(newAnalyzer);
+		Utils.checkNotNull(scope);
 		this.oldAnalyzer = oldAnalyzer;
 		this.newAnalyzer = newAnalyzer;
-		this.matcher = new MethodMatcher(oldAnalyzer.getCallGraph(), newAnalyzer.getCallGraph());
+		this.matcher = new MethodMatcher(oldAnalyzer.getCallGraph(), newAnalyzer.getCallGraph(), scope);
+		this.scope = scope;
 	}
 	
 	public void setThreshold(float d) {
@@ -44,16 +49,19 @@ public class MethodMatchingLogics {
 	}
 	
 	public List<CGNode> getMatchedMethods(CGNode oldNode) {
-		Utils.checkTrue(this.oldAnalyzer.hasNode(oldNode));
+		Utils.checkTrue(this.oldAnalyzer.hasNode(oldNode), "Old node is: " + oldNode.getMethod().getSignature());
 		List<CGNode> nodeList = new LinkedList<CGNode>();
 		
 		CGNode exactMatchedNode = this.matcher.getMethodInNewCG(oldNode.getMethod().getSignature());
 		if(exactMatchedNode != null) {
 			nodeList.add(exactMatchedNode);
 		} else {
-			//use the fuzzing matching
-			List<CGNode> fuzzMatchedNodes = this.matcher.getMatchedNodes(oldNode, threshold, lookahead);
-			nodeList.addAll(fuzzMatchedNodes);
+			if(USE_FUZZING_MATCHING) {
+			    //use the fuzzing matching
+			    List<CGNode> fuzzMatchedNodes
+			        = this.matcher.getMatchedNodes(oldNode, threshold, lookahead);
+			    nodeList.addAll(fuzzMatchedNodes);
+			}
 		}
 		
 		return nodeList;
