2c4d88235e62105861455ad6b14cd2f69539898f
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2013-06-06 23:49:40 +0000

    the randoop test is still too slow

--- trunk/configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestPredicateAnalyzer.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestPredicateAnalyzer.java
@@ -10,6 +10,8 @@ import junit.framework.TestCase;
 public class TestPredicateAnalyzer extends TestCase {
 
 	public void testRandoop() {
+		MethodMatcher.debug = false;
+		
 		CodeAnalyzer oldAnalyzer = CodeAnalyzerRepository.getRandoop121Analyzer();
 		oldAnalyzer.buildAnalysis();
 		CodeAnalyzer newAnalyzer = CodeAnalyzerRepository.getRandoop132Analyzer();
@@ -17,13 +19,15 @@ public class TestPredicateAnalyzer extends TestCase {
 		String oldTraceFile = "./evol-experiments/randoop/randoop-1.2.1.txt";
 		String newTraceFile = "./evol-experiments/randoop/randoop-1.3.2.txt";
 		AnalysisScope scope = AnalysisScopeRepository.createRandoopScore();
+		AnalysisCache cache = AnalysisCache.createCache(oldAnalyzer, newAnalyzer, scope);
+		
 		//analyze the predicate
 		PredicateExecInfoAnalyzer analyzer
-		    = new PredicateExecInfoAnalyzer(oldAnalyzer, newAnalyzer, scope,
+		    = new PredicateExecInfoAnalyzer(oldAnalyzer, newAnalyzer, scope, cache,
 				oldTraceFile, newTraceFile);
 		
 		//only exact matching
-		MethodMatchingLogics.USE_FUZZING_MATCHING = false;
+//		MethodMatchingLogics.USE_FUZZING_MATCHING = false;
 		
 		//find the deviation pairs
 		Map<Pair<PredicateExecInfo, PredicateExecInfo>, Float> predicatePairs
@@ -36,15 +40,24 @@ public class TestPredicateAnalyzer extends TestCase {
 			PredicateExecInfo newP = p.b;
 			System.out.println(oldP + " ==> " + newP);
 			System.out.println("   " + predicatePairs.get(p));
-			p.a.showContext(oldAnalyzer.getCallGraph());
-			p.b.showContext(newAnalyzer.getCallGraph());
 			
-			System.out.println("---------------------");
-			break;
+			//only for experiment
+//			p.a.showContext(oldAnalyzer.getCallGraph());
+//			p.b.showContext(newAnalyzer.getCallGraph());
+//			System.out.println("---------------------");
+			
+//			for(Pair<PredicateExecInfo, PredicateExecInfo> pair : predicatePairs.keySet()) {
+//				if(pair.a == p.a) {
+//					System.out.println("=== " + pair.b);
+//				}
+//			}
+//			
+//			break;
 		}
  	}
 	
 	public void tearDown() {
 		MethodMatchingLogics.USE_FUZZING_MATCHING = true;
+		MethodMatcher.debug = true;
 	}
 }
