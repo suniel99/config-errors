078117681cf02b8b5debe90fb1a60bab77c50e8f
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2013-06-25 16:16:18 +0000

    the chord subject program needs re-compliation

--- trunk/configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestPredicateAnalyzer.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestPredicateAnalyzer.java
@@ -60,7 +60,82 @@ public class TestPredicateAnalyzer extends TestCase {
  	}
 	
 	public void testSynoptic() {
+		boolean matchDebug = false;
+		boolean fuzzMatching = false;
+		CodeAnalyzer oldAnalyzer = CodeAnalyzerRepository.getSynopticOldAnalyzer();
+		CodeAnalyzer newAnalyzer = CodeAnalyzerRepository.getSynopticNewAnalyzer();
+		String oldTraceFile = TraceRepository.synopticOldTrace;
+		String newTraceFile = TraceRepository.synopticNewTrace;
+		AnalysisScope scope = AnalysisScopeRepository.createSynopticScope();
+		this.findDeviatedPredicates(matchDebug, fuzzMatching, oldAnalyzer, newAnalyzer, oldTraceFile, newTraceFile, scope);
+	}
+	
+	//FIXME, the chord bytecode needs to be re-compiled, seems
+	public void testJChord() {
+		boolean matchDebug = false;
+		boolean fuzzMatching = false;
+		CodeAnalyzer oldAnalyzer = CodeAnalyzerRepository.getJChordOldAnalyzer();
+		CodeAnalyzer newAnalyzer = CodeAnalyzerRepository.getJChordNewAnalyzer();
+		String oldTraceFile = TraceRepository.jchordP1OldTrace;
+		String newTraceFile = TraceRepository.jchordP1NewTrace;
+		AnalysisScope scope = AnalysisScopeRepository.createJChordScope();
+		this.findDeviatedPredicates(matchDebug, fuzzMatching, oldAnalyzer, newAnalyzer, oldTraceFile, newTraceFile, scope);
 		
+		oldTraceFile = TraceRepository.jchordP2OldTrace;
+		newTraceFile = TraceRepository.jchordP2NewTrace;
+		this.findDeviatedPredicates(matchDebug, fuzzMatching, oldAnalyzer, newAnalyzer, oldTraceFile, newTraceFile, scope);
+	}
+	
+	public void testJMeter() {
+		boolean matchDebug = false;
+		boolean fuzzMatching = false;
+		CodeAnalyzer oldAnalyzer = CodeAnalyzerRepository.getJMeterOldAnalyzer();
+		CodeAnalyzer newAnalyzer = CodeAnalyzerRepository.getJMeterNewAnalyzer();
+		String oldTraceFile = TraceRepository.jmeterOldTrace;
+		String newTraceFile = TraceRepository.jmeterNewTrace;
+		AnalysisScope scope = AnalysisScopeRepository.createJMeterScope();
+		this.findDeviatedPredicates(matchDebug, fuzzMatching, oldAnalyzer, newAnalyzer, oldTraceFile, newTraceFile, scope);
+	}
+	
+	public void testWeka() {
+		boolean matchDebug = false;
+		boolean fuzzMatching = false;
+		CodeAnalyzer oldAnalyzer = CodeAnalyzerRepository.getWekaOldAnalyzer();
+		CodeAnalyzer newAnalyzer = CodeAnalyzerRepository.getWekaNewAnalyzer();
+		String oldTraceFile = TraceRepository.wekaOldTrace;
+		String newTraceFile = TraceRepository.wekaNewTrace;
+		AnalysisScope scope = AnalysisScopeRepository.createWekaScope();
+		this.findDeviatedPredicates(matchDebug, fuzzMatching, oldAnalyzer, newAnalyzer, oldTraceFile, newTraceFile, scope);
+	}
+	
+	public void findDeviatedPredicates(boolean matchDebug, boolean fuzzMatching, CodeAnalyzer oldAnalyzer,
+			CodeAnalyzer newAnalyzer, String oldTraceFile, String newTraceFile, AnalysisScope scope) {
+		MethodMatcher.debug = matchDebug;
+		
+		oldAnalyzer.buildAnalysis();
+		newAnalyzer.buildAnalysis();
+		AnalysisCache cache = AnalysisCache.createCache(oldAnalyzer, newAnalyzer, scope);
+		
+		//analyze the predicate
+		PredicateExecInfoAnalyzer analyzer
+		    = new PredicateExecInfoAnalyzer(oldAnalyzer, newAnalyzer, scope, cache,
+				oldTraceFile, newTraceFile);
+		
+		//only exact matching
+		MethodMatchingLogics.USE_FUZZING_MATCHING = fuzzMatching;
+		
+		//find the deviation pairs
+		Map<Pair<PredicateExecInfo, PredicateExecInfo>, Float> predicatePairs
+		    = analyzer.findBehaviorDeviatedPredicatePairs();
+		
+		predicatePairs = Utils.sortByValue(predicatePairs, false);
+		
+		for(Pair<PredicateExecInfo, PredicateExecInfo> p : predicatePairs.keySet()) {
+			PredicateExecInfo oldP = p.a;
+			PredicateExecInfo newP = p.b;
+			System.out.println(oldP + " ==> " + newP);
+			System.out.println("   " + predicatePairs.get(p));
+		}
 	}
 	
 	public void tearDown() {
