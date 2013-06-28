99ad38490694bbb204fb77077cace9be8cf0ec55
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2013-06-28 22:41:37 +0000

    test finding dominators

---
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestFindPostDominator.java
@@ -0,0 +1,48 @@
+package edu.washington.cs.conf.analysis.evol;
+
+import java.util.Collection;
+
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ssa.ISSABasicBlock;
+
+import edu.washington.cs.conf.util.WALAUtils;
+import junit.framework.TestCase;
+
+public class TestFindPostDominator extends TestCase {
+	
+	public void testPostDomExampleFoo() {
+		CodeAnalysisUtils.post_dom_debug = false;
+		this.showPostDomExample("test.evol.examples.PostDominate.foo");
+	}
+
+    public void testPostDomExampleBar() {
+    	CodeAnalysisUtils.post_dom_debug = false;
+		this.showPostDomExample("test.evol.examples.PostDominate.bar");
+	}
+    
+    public void testPostDomExampleLoop() {
+    	CodeAnalysisUtils.post_dom_debug = false;
+		this.showPostDomExample("test.evol.examples.PostDominate.loop");
+	}
+	
+	private void showPostDomExample(String methodName) {
+		String classpath = "./bin/test/evol/examples";
+		String mainMethod = "Ltest/evol/examples/PostDominate";
+		CodeAnalyzer coder = new CodeAnalyzer(classpath, mainMethod);
+		coder.buildAnalysis();
+		
+		Collection<CGNode> nodes = WALAUtils.lookupCGNode(coder.getCallGraph(), methodName);
+		System.out.println(nodes);
+		
+		CGNode node = nodes.iterator().next();
+		
+		WALAUtils.printCFG(node);
+		
+		for(ISSABasicBlock bb : WALAUtils.getAllBasicBlocks(node)) {
+			ISSABasicBlock postbb = CodeAnalysisUtils.computeImmediatePostDominator(node, bb);
+			System.out.println("The immediate post of: " + bb.getNumber() + " is: " + postbb);
+			System.out.println("------------");
+		}
+	}
+	
+}
