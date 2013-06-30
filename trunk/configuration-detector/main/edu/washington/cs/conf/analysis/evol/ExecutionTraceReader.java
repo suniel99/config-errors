fdabd854c2ef16d78901aa50ef3113038790d808
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2013-06-30 02:35:18 +0000

    read execution traces from file

---
+++ trunk/configuration-detector/main/edu/washington/cs/conf/analysis/evol/ExecutionTraceReader.java
@@ -0,0 +1,77 @@
+package edu.washington.cs.conf.analysis.evol;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.washington.cs.conf.instrument.evol.EfficientTracer;
+import edu.washington.cs.conf.util.Files;
+import edu.washington.cs.conf.util.Utils;
+
+public class ExecutionTraceReader {
+	
+	//a sample line
+	//NORMAL:randoop.util.Reflection.isVisible(Ljava/lang/Class;)Z##11
+	public static InstructionExecInfo createInstructionExecInfo(String line) {
+		InstructionExecInfo execInfo = null;
+		String[] splits = line.split(EfficientTracer.SEP);
+		Utils.checkTrue(splits.length == 2);
+		Integer index = Integer.parseInt(splits[1]);
+		String other = splits[0];
+		if(other.startsWith(EfficientTracer.NORMAL)) {
+			String context = other.substring(EfficientTracer.NORMAL.length());
+			execInfo = new InstructionExecInfo(context, index);
+		} else if (other.startsWith(EfficientTracer.EVAL)) {
+			String context = other.substring(EfficientTracer.EVAL.length());
+			execInfo = new BranchInstructionExecInfo(context, index, true);
+		} else if (other.startsWith(EfficientTracer.EXEC)) {
+			String context = other.substring(EfficientTracer.EXEC.length());
+			execInfo = new BranchInstructionExecInfo(context, index, false);
+		} else {
+			throw new Error("Invalid: " + line);
+		}
+		return execInfo;
+	}
+	
+	public static List<InstructionExecInfo> createInstructionExecInfoList(String fileName) {
+		List<InstructionExecInfo> list = new LinkedList<InstructionExecInfo>();
+		List<String> fileContent = Files.readWholeNoExp(fileName);
+		for(String line : fileContent) {
+			if(line.trim().isEmpty()) {
+				continue;
+			}
+			InstructionExecInfo execInfo = createInstructionExecInfo(line);
+			list.add(execInfo);
+		}
+		return list;
+	}
+
+	//a sample line:
+	//randoop.util.Reflection.isVisible(Ljava/lang/Class;)Z##11==1:0
+	public static PredicateExecInfo createPredicateExecInfo(String line) {
+		Utils.checkNotNull(line);
+		String[] splits = line.split(EfficientTracer.SEP);
+		String context = splits[0];
+		String[] indexAndEval = splits[1].split(EfficientTracer.PRED_SEP);
+		String predicate = indexAndEval[0];
+		String[] results = indexAndEval[1].split(EfficientTracer.EVAL_SEP);
+		Integer freq = Integer.parseInt(results[0]);
+		Integer eval = Integer.parseInt(results[1]);
+		Utils.checkTrue(freq >= eval);
+		return new PredicateExecInfo(context, predicate, freq, eval);
+	}
+	
+	public static Collection<PredicateExecInfo> createPredicateExecInfoList(String fileName) {
+		Collection<PredicateExecInfo> coll = new LinkedList<PredicateExecInfo>();
+		List<String> fileContent = Files.readWholeNoExp(fileName);
+		for(String line : fileContent) {
+			if(line.trim().isEmpty()) {
+				continue;
+			}
+			PredicateExecInfo execInfo = createPredicateExecInfo(line);
+			coll.add(execInfo);
+		}
+		return coll;
+	}
+	
+}
