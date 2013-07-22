4999c649e329c767ea02f804f977f34d5170efd5
racezhang <racezhang@gmail.com>
2013-07-22 21:58:42 +0000

    test on small programs

--- configuration-detector/tests/edu/washington/cs/conf/instrument/evol/TestPredicateInstrumenter.java
+++ configuration-detector/tests/edu/washington/cs/conf/instrument/evol/TestPredicateInstrumenter.java
@@ -5,10 +5,14 @@ import junit.framework.TestCase;
 
 public class TestPredicateInstrumenter extends TestCase {
 
+	//export the tracer jar as evoltracer.jar, and execute the test driver, via:
+	// java -cp ./output.jar;./evoltracer.jar test.slice.depfield.SeeCoverage n1 n2
 	public void testSimpleInstrumenter() throws Exception {
 		PredicateInstrumenter instrumenter = new PredicateInstrumenter();
 		instrumenter.setDisasm(true);
+		instrumenter.setUseSigMap(true);
 		instrumenter.instrument("./subjects/testdata.jar", "./output.jar");
+		instrumenter.saveSigMappings();
 		InstrumentStats.showInstrumentationStats();
 	}
 	
