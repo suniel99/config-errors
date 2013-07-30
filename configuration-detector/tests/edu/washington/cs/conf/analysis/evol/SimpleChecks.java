382fedccbb27f8b795047c16c2ba0aeeba7c568c
racezhang <racezhang@gmail.com>
2013-07-30 23:52:32 +0000

    a class performing simple checks

---
+++ configuration-detector/tests/edu/washington/cs/conf/analysis/evol/SimpleChecks.java
@@ -0,0 +1,40 @@
+package edu.washington.cs.conf.analysis.evol;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.washington.cs.conf.analysis.evol.experimental.PredicateExecInfo;
+import edu.washington.cs.conf.util.Utils;
+
+//just for experiment purpose
+public class SimpleChecks {
+
+	public static Set<String> getUnmatchedOldMethods(
+			Collection<PredicateExecInfo> oldSet,
+			Collection<PredicateExecInfo> newSet) {
+		Set<String> oldMethods = new HashSet<String>();
+		for(PredicateExecInfo execInfo : oldSet) {
+			oldMethods.add(execInfo.context);
+		}
+		Set<String> newMethods = new HashSet<String>();
+		for(PredicateExecInfo execInfo : newSet) {
+			newMethods.add(execInfo.context);
+		}
+		return Utils.minus(oldMethods, newMethods);
+	}
+	
+	public static Set<String> getUnmatchedNewMethods(
+			Collection<PredicateExecInfo> oldSet,
+			Collection<PredicateExecInfo> newSet) {
+		Set<String> oldMethods = new HashSet<String>();
+		for(PredicateExecInfo execInfo : oldSet) {
+			oldMethods.add(execInfo.context);
+		}
+		Set<String> newMethods = new HashSet<String>();
+		for(PredicateExecInfo execInfo : newSet) {
+			newMethods.add(execInfo.context);
+		}
+		return Utils.minus(newMethods, oldMethods);
+	}
+}
