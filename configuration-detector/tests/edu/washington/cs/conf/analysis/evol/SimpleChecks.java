61fe0d67184a19da491b22cf6088a5fe61e9c969
racezhang <racezhang@gmail.com>
2013-08-02 02:48:44 +0000

    Update tests

--- configuration-detector/tests/edu/washington/cs/conf/analysis/evol/SimpleChecks.java
+++ configuration-detector/tests/edu/washington/cs/conf/analysis/evol/SimpleChecks.java
@@ -2,6 +2,8 @@ package edu.washington.cs.conf.analysis.evol;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 import edu.washington.cs.conf.analysis.evol.experimental.PredicateExecInfo;
@@ -36,5 +38,67 @@ public class SimpleChecks {
 			newMethods.add(execInfo.context);
 		}
 		return Utils.minus(newMethods, oldMethods);
+	}
+	
+	public static Set<String> getUnmatchedOldPredicates(
+			Collection<PredicateExecInfo> oldSet,
+			Collection<PredicateExecInfo> newSet
+	    ) {
+		Set<String> oldPredicates = new HashSet<String>();
+		for(PredicateExecInfo execInfo : oldSet) {
+			oldPredicates.add(execInfo.getPredicateSig());
+		}
+		Set<String> newPredicates = new HashSet<String>();
+		for(PredicateExecInfo execInfo : newSet) {
+			newPredicates.add(execInfo.getPredicateSig());
+		}
+		return Utils.minus(oldPredicates, newPredicates);
+	}
+	
+	public static Set<String> getUnmatchedNewPredicates(
+			Collection<PredicateExecInfo> oldSet,
+			Collection<PredicateExecInfo> newSet
+	    ) {
+		Set<String> oldPredicates = new HashSet<String>();
+		for(PredicateExecInfo execInfo : oldSet) {
+			oldPredicates.add(execInfo.getPredicateSig());
+		}
+		Set<String> newPredicates = new HashSet<String>();
+		for(PredicateExecInfo execInfo : newSet) {
+			newPredicates.add(execInfo.getPredicateSig());
+		}
+		return Utils.minus(newPredicates, oldPredicates);
+	}
+	
+	//get behaviorally changed predicates
+	public static Set<PredicateBehaviorAcrossVersions> getMatchedPredicateExecutions(
+			Collection<PredicateExecInfo> oldSet,
+			Collection<PredicateExecInfo> newSet
+			) {
+		
+		Map<String, PredicateExecInfo> oldPredMap = new LinkedHashMap<String, PredicateExecInfo>();
+		for(PredicateExecInfo oldPred : oldSet) {
+			oldPredMap.put(oldPred.getPredicateSig(), oldPred);
+		}
+		
+		Map<String, PredicateExecInfo> newPredMap = new LinkedHashMap<String, PredicateExecInfo>();
+		for(PredicateExecInfo newPred : newSet) {
+			newPredMap.put(newPred.getPredicateSig(), newPred);
+		}
+		
+		Set<PredicateBehaviorAcrossVersions> predSet = new HashSet<PredicateBehaviorAcrossVersions>();
+		for(String oldPredSig : oldPredMap.keySet()) {
+			if(newPredMap.containsKey(oldPredSig)) {
+				PredicateExecInfo oldExecInfo = oldPredMap.get(oldPredSig);
+				PredicateExecInfo newExecInfo = newPredMap.get(oldPredSig);
+				//create a predicate execution object
+				PredicateBehaviorAcrossVersions execObj = new PredicateBehaviorAcrossVersions(oldExecInfo.getMethodSig(), oldExecInfo.getIndex());
+				execObj.setOldExecutionInfo(oldExecInfo.evalFreqCount, oldExecInfo.evalResultCount);
+				execObj.setNewExecutionInfo(newExecInfo.evalFreqCount, newExecInfo.evalResultCount);
+				predSet.add(execObj);
+			}
+		}
+		
+		return predSet;
 	}
 }
--- configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestParseExecInstructionInfo.java
+++ configuration-detector/tests/edu/washington/cs/conf/analysis/evol/TestParseExecInstructionInfo.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.Set;
 
 import edu.washington.cs.conf.analysis.evol.experimental.PredicateExecInfo;
+import edu.washington.cs.conf.util.Utils;
 
 import junit.framework.TestCase;
 
 public class TestParseExecInstructionInfo extends TestCase {
 
-	public void testParseRandoop() {
+	public void testParseRandoop_predicate() {
 		Collection<PredicateExecInfo>  oldPredExecs
            = ExecutionTraceReader.createPredicateExecInfoList(TraceRepository.randoopOldPredicateDump,
         		TraceRepository.randoopOldSig);
@@ -23,9 +24,73 @@ public class TestParseExecInstructionInfo extends TestCase {
 //	    System.out.println(oldUnmatchedMethods);
 	    System.out.println("Number of unmatched new methods: " + newUnmatchedMethods.size());
 //	    System.out.println(newUnmatchedMethods);
+	    
+	    Set<String> oldUnmatchedPredicates = SimpleChecks.getUnmatchedOldPredicates(oldPredExecs, newPredExecs);
+		Set<String> newUnmatchedPredicates = SimpleChecks.getUnmatchedNewPredicates(oldPredExecs, newPredExecs);
+		System.out.println("Number of unmatched old predicates: " + oldUnmatchedPredicates.size());
+		System.out.println("Number of unmatched new predicates: " + newUnmatchedPredicates.size());
+		
+		Set<PredicateBehaviorAcrossVersions> matchedPreds = SimpleChecks.getMatchedPredicateExecutions(oldPredExecs, newPredExecs);
+		System.out.println("Number of matched preds: " + matchedPreds.size());
 	}
 	
-	public void testParseSynoptic() {
+	public void testParseRandoop_trace() {
+		List<InstructionExecInfo> oldInstructions
+		    = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.randoopOldHistoryDump,
+		    		TraceRepository.randoopOldSig);
+		List<InstructionExecInfo> newInstructions
+	        = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.randoopNewHistoryDump,
+	        		TraceRepository.randoopNewSig);
+		System.out.println("old instruction num: " + oldInstructions.size());
+		System.out.println("new instruction num: " + newInstructions.size());
+	}
+	
+	
+	public void testCheckRandoop_predicate_in_old_trace() {
+		CodeAnalyzer oldCoder = CodeAnalyzerRepository.getRandoop121Analyzer();
+		oldCoder.buildAnalysis();
+		
+		//old trace
+		ExecutionTrace oldTrace = new ExecutionTrace(TraceRepository.randoopOldHistoryDump, 
+				TraceRepository.randoopOldSig, TraceRepository.randoopOldPredicateDump);
+		Set<PredicateExecInfo> predSet = oldTrace.getExecutedPredicates();
+		System.out.println("Num of executed predicates in old trace: " + predSet.size());
+		//for each predicate, get its immediate post-dominator
+		for(PredicateExecInfo pred : predSet) {
+			InstructionExecInfo postDomExec = oldTrace.getImmediatePostDominator(oldCoder, pred);
+			if(postDomExec == null) {
+				continue;
+			}
+			Utils.checkTrue(pred.getIndex() != postDomExec.getIndex());
+			System.out.println("Pred: " + pred);
+			System.out.println("     post dom: " + postDomExec);
+		}
+	}
+	
+	public void testCheckRandoop_predicate_in_new_trace() {
+		CodeAnalyzer newCoder = CodeAnalyzerRepository.getRandoop132Analyzer();
+		newCoder.buildAnalysis();
+		
+		//old trace
+		ExecutionTrace oldTrace = new ExecutionTrace(TraceRepository.randoopNewHistoryDump, 
+				TraceRepository.randoopNewSig, TraceRepository.randoopNewPredicateDump);
+		Set<PredicateExecInfo> predSet = oldTrace.getExecutedPredicates();
+		System.out.println("Num of executed predicates in new trace: " + predSet.size());
+		//for each predicate, get its immediate post-dominator
+		for(PredicateExecInfo pred : predSet) {
+			InstructionExecInfo postDomExec = oldTrace.getImmediatePostDominator(newCoder, pred);
+			if(postDomExec == null) {
+				continue;
+			}
+			Utils.checkTrue(pred.getIndex() != postDomExec.getIndex());
+			System.out.println("Pred: " + pred);
+			System.out.println("     post dom: " + postDomExec);
+		}
+	}
+	
+	//---------------------------
+	
+	public void testParseSynoptic_predicate() {
 		Collection<PredicateExecInfo>  oldPredExecs
 	        = ExecutionTraceReader.createPredicateExecInfoList(TraceRepository.synopticOldPredicateDump,
 	        		TraceRepository.synopticOldSig);
@@ -35,12 +100,69 @@ public class TestParseExecInstructionInfo extends TestCase {
 		Set<String> oldUnmatchedMethods = SimpleChecks.getUnmatchedOldMethods(oldPredExecs, newPredExecs);
 		Set<String> newUnmatchedMethods = SimpleChecks.getUnmatchedNewMethods(oldPredExecs, newPredExecs);
 		System.out.println("Number of unmatched old methods: " + oldUnmatchedMethods.size());
-		System.out.println(oldUnmatchedMethods);
+//		System.out.println(oldUnmatchedMethods);
 		System.out.println("Number of unmatched new methods: " + newUnmatchedMethods.size());
-		System.out.println(newUnmatchedMethods);
+//		System.out.println(newUnmatchedMethods);
+		
+		Set<String> oldUnmatchedPredicates = SimpleChecks.getUnmatchedOldPredicates(oldPredExecs, newPredExecs);
+		Set<String> newUnmatchedPredicates = SimpleChecks.getUnmatchedNewPredicates(oldPredExecs, newPredExecs);
+		System.out.println("Number of unmatched old predicates: " + oldUnmatchedPredicates.size());
+		System.out.println("Number of unmatched new predicates: " + newUnmatchedPredicates.size());
+		
+		Set<PredicateBehaviorAcrossVersions> matchedPreds = SimpleChecks.getMatchedPredicateExecutions(oldPredExecs, newPredExecs);
+		System.out.println("Number of matched preds: " + matchedPreds.size());
 	}
 	
-	public void testParseWeka() {
+	public void testParseSynoptic_trace() {
+		List<InstructionExecInfo> oldInstructions
+		    = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.synopticOldHistoryDump,
+		    		TraceRepository.synopticOldSig);
+		List<InstructionExecInfo> newInstructions
+	        = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.synopticNewHistoryDump,
+	        		TraceRepository.synopticNewSig);
+		System.out.println("old instruction num: " + oldInstructions.size());
+		System.out.println("new instruction num: " + newInstructions.size());
+	}
+	
+	public void testCheckSynoptic_predicate_in_old_trace() {
+		CodeAnalyzer oldCoder = CodeAnalyzerRepository.getSynopticOldAnalyzer();
+		oldCoder.buildAnalysis();
+		
+		//old trace
+		ExecutionTrace oldTrace = new ExecutionTrace(TraceRepository.synopticOldHistoryDump, 
+				TraceRepository.synopticOldSig, TraceRepository.synopticOldPredicateDump);
+		Set<PredicateExecInfo> predSet = oldTrace.getExecutedPredicates();
+		System.out.println("Num of executed predicates in old trace: " + predSet.size());
+		//for each predicate, get its immediate post-dominator
+		for(PredicateExecInfo pred : predSet) {
+			InstructionExecInfo postDomExec = oldTrace.getImmediatePostDominator(oldCoder, pred);
+			Utils.checkTrue(pred.getIndex() != postDomExec.getIndex());
+			System.out.println("Pred: " + pred);
+			System.out.println("     post dom: " + postDomExec);
+		}
+	}
+	
+	
+	public void testCheckSynoptic_predicate_in_new_trace() {
+		CodeAnalyzer newCoder = CodeAnalyzerRepository.getSynopticNewAnalyzer();
+		newCoder.buildAnalysis();
+		//new trace
+		ExecutionTrace newTrace = new ExecutionTrace(TraceRepository.synopticNewHistoryDump, 
+				TraceRepository.synopticNewSig, TraceRepository.synopticNewPredicateDump);
+		Set<PredicateExecInfo> predSet = newTrace.getExecutedPredicates();
+		System.out.println("Num of executed predicates in old trace: " + predSet.size());
+		//for each predicate, get its immediate post-dominator
+		for(PredicateExecInfo pred : predSet) {
+			InstructionExecInfo postDomExec = newTrace.getImmediatePostDominator(newCoder, pred);
+			Utils.checkTrue(pred.getIndex() != postDomExec.getIndex());
+			System.out.println("Pred: " + pred);
+			System.out.println("     post dom: " + postDomExec);
+		}
+	}
+	
+	//----------------------------------
+	
+	public void testParseWeka_predicate() {
 		Collection<PredicateExecInfo>  oldPredExecs
           = ExecutionTraceReader.createPredicateExecInfoList(TraceRepository.wekaOldPredicateDump,
 		    TraceRepository.wekaOldSig);
@@ -54,10 +176,67 @@ public class TestParseExecInstructionInfo extends TestCase {
         System.out.println(oldUnmatchedMethods);
         System.out.println("Number of unmatched new methods: " + newUnmatchedMethods.size());
         System.out.println(newUnmatchedMethods);
+        
+        Set<String> oldUnmatchedPredicates = SimpleChecks.getUnmatchedOldPredicates(oldPredExecs, newPredExecs);
+		Set<String> newUnmatchedPredicates = SimpleChecks.getUnmatchedNewPredicates(oldPredExecs, newPredExecs);
+		System.out.println("Number of unmatched old predicates: " + oldUnmatchedPredicates.size());
+		System.out.println("Number of unmatched new predicates: " + newUnmatchedPredicates.size());
+		
+		Set<PredicateBehaviorAcrossVersions> matchedPreds = SimpleChecks.getMatchedPredicateExecutions(oldPredExecs, newPredExecs);
+		System.out.println("Number of matched preds: " + matchedPreds.size());
 	}
 	
+	public void testParseWeka_trace() {
+		List<InstructionExecInfo> oldInstructions
+		    = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.wekaOldHistoryDump,
+		    		TraceRepository.wekaOldSig);
+		List<InstructionExecInfo> newInstructions
+	        = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.wekaNewHistoryDump,
+	        		TraceRepository.wekaNewSig);
+		System.out.println("old instruction num: " + oldInstructions.size());
+		System.out.println("new instruction num: " + newInstructions.size());
+	}
+	
+	public void testCheckWeka_predicate_in_old_trace() {
+		CodeAnalyzer oldCoder = CodeAnalyzerRepository.getWekaOldAnalyzer();
+		oldCoder.buildAnalysis();
+		
+		//old trace
+		ExecutionTrace oldTrace = new ExecutionTrace(TraceRepository.wekaOldHistoryDump, 
+				TraceRepository.wekaOldSig, TraceRepository.wekaOldPredicateDump);
+		Set<PredicateExecInfo> predSet = oldTrace.getExecutedPredicates();
+		System.out.println("Num of executed predicates in old trace: " + predSet.size());
+		//for each predicate, get its immediate post-dominator
+		for(PredicateExecInfo pred : predSet) {
+			InstructionExecInfo postDomExec = oldTrace.getImmediatePostDominator(oldCoder, pred);
+			Utils.checkTrue(pred.getIndex() != postDomExec.getIndex());
+			System.out.println("Pred: " + pred);
+			System.out.println("     post dom: " + postDomExec);
+		}
+	}
+	
+	public void testCheckWeka_predicate_in_new_trace() {
+		CodeAnalyzer oldCoder = CodeAnalyzerRepository.getWekaNewAnalyzer();
+		oldCoder.buildAnalysis();
+		
+		//old trace
+		ExecutionTrace newTrace = new ExecutionTrace(TraceRepository.wekaNewHistoryDump, 
+				TraceRepository.wekaNewSig, TraceRepository.wekaNewPredicateDump);
+		Set<PredicateExecInfo> predSet = newTrace.getExecutedPredicates();
+		System.out.println("Num of executed predicates in new trace: " + predSet.size());
+		//for each predicate, get its immediate post-dominator
+		for(PredicateExecInfo pred : predSet) {
+			InstructionExecInfo postDomExec = newTrace.getImmediatePostDominator(oldCoder, pred);
+			Utils.checkTrue(pred.getIndex() != postDomExec.getIndex());
+			System.out.println("Pred: " + pred);
+			System.out.println("     post dom: " + postDomExec);
+		}
+	}
+	
+	//--------------------------------------
+	
 	//need to analyze different behaviors of the predicate
-	public void testParseJMeter() {
+	public void testParseJMeter_predicate() {
 		Collection<PredicateExecInfo>  oldPredExecs
             = ExecutionTraceReader.createPredicateExecInfoList(TraceRepository.jmeterOldPredicateDump,
     		    TraceRepository.jmeterOldSig);
@@ -70,5 +249,26 @@ public class TestParseExecInstructionInfo extends TestCase {
 	    System.out.println(oldUnmatchedMethods);
 	    System.out.println("Number of unmatched new methods: " + newUnmatchedMethods.size());
         System.out.println(newUnmatchedMethods);
+        
+        Set<String> oldUnmatchedPredicates = SimpleChecks.getUnmatchedOldPredicates(oldPredExecs, newPredExecs);
+		Set<String> newUnmatchedPredicates = SimpleChecks.getUnmatchedNewPredicates(oldPredExecs, newPredExecs);
+		System.out.println("Number of unmatched old predicates: " + oldUnmatchedPredicates.size());
+		System.out.println("Number of unmatched new predicates: " + newUnmatchedPredicates.size());
+		
+
+		Set<PredicateBehaviorAcrossVersions> matchedPreds = SimpleChecks.getMatchedPredicateExecutions(oldPredExecs, newPredExecs);
+		System.out.println("Number of matched preds: " + matchedPreds.size());
+	}
+	
+	public void testParseJMeter_trace() {
+		List<InstructionExecInfo> oldInstructions
+		    = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.jmeterOldHistoryDump,
+		    		TraceRepository.jmeterOldSig);
+		List<InstructionExecInfo> newInstructions
+	        = ExecutionTraceReader.createPredicateExecInfoInTrace(TraceRepository.jmeterNewHistoryDump,
+	        		TraceRepository.jmeterNewSig);
+		System.out.println("old instruction num: " + oldInstructions.size());
+		System.out.println("new instruction num: " + newInstructions.size());
+		
 	}
 }
