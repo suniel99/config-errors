73f0defb0fdd76c1b70b77e18b6fe34762b34f44
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2014-07-03 23:20:38 +0000

    mutated config options

--- trunk/configuration-detector/main/edu/washington/cs/conf/mutation/ConfMutator.java
+++ trunk/configuration-detector/main/edu/washington/cs/conf/mutation/ConfMutator.java
@@ -1,11 +1,108 @@
 package edu.washington.cs.conf.mutation;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import edu.washington.cs.conf.mutation.ConfValueTypeInferrer.ConfType;
+import edu.washington.cs.conf.util.Files;
+import edu.washington.cs.conf.util.Globals;
+import edu.washington.cs.conf.util.Utils;
+
 /**
  * Mutate the configuration file, and write the mutated conf to the disk
  * */
 public class ConfMutator {
 
+	private final ConfParser parser;
+	
+	private final ValueGenerator valueGenerator = new ValueGenerator();
+	
 	//mutate each part of the configuration property file, and output
 	//the mutated results to disk
 	
+	public ConfMutator(String filePath) {
+		this.parser = new ConfParser(filePath);
+		this.parser.parseFile();
+	}
+	
+	public List<MutatedConf> mutateConfFile() {
+		List<MutatedConf> mutatedConfList = new LinkedList<MutatedConf>();
+		for(String optionName : this.parser.getOptions()) {
+			String origValue = this.parser.getValue(optionName);
+			List<String> mutatedValues = this.createMutatedValues(optionName);
+			Utils.removeRedundant(mutatedValues);
+			//set the mutated optionName
+//			System.out.println("size of mutated values: " + mutatedValues.size());
+			for(String mutatedValue : mutatedValues) {
+				//if the mutated value is the same as the original one
+				if(mutatedValue.equals(origValue)) {
+					continue;
+				}
+			    Map<String, String> optionValues = this.parser.getOptionValueMap();
+			    optionValues.put(optionName, mutatedValue); //mutated the value
+			    MutatedConf mutatedConf = new MutatedConf(optionValues, optionName, origValue);
+			    //add to the list
+			    mutatedConfList.add(mutatedConf);
+			}
+		}
+		return mutatedConfList;
+	}
+	
+	//start to mutate the read conf file
+	//produce a list of values
+	public List<String> createMutatedValues(String optionName) {
+		List<String> arrayList = new ArrayList<String>();
+		
+		//start to mutate
+		ConfType type = this.parser.getType(optionName);
+		String currValue = this.parser.getValue(optionName);
+		//the mutated values
+		List<Object> mutatedValues = this.valueGenerator.generateMutatedValues(currValue, type);
+//		
+//		System.out.println("Mutating: " + optionName + ", type: " + type);
+//		System.out.println(" Original value: " + currValue);
+//		System.out.println(" Mutated values: " + mutatedValues);
+//		
+		for(Object v : mutatedValues) {
+			arrayList.add(v.toString());
+		}
+		
+		return arrayList;
+	}
+	
+	public static void writeConfFile(String filePath, Map<String, String> confValues) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(String option : confValues.keySet()) {
+			sb.append(option);
+			sb.append("=");
+			sb.append(confValues.get(option));
+			sb.append(Globals.lineSep);
+		}
+		
+		Files.writeToFileNoExp(sb.toString(), filePath);
+	}
+	
+	public static void main(String[] args) {
+		String filePath = "./sample-config-files/jmeter.properties";
+		ConfMutator mutator = new ConfMutator(filePath);
+		List<MutatedConf> mutatedConfList = mutator.mutateConfFile();
+		System.out.println(mutatedConfList.size());
+//		for(String optionName : mutator.parser.getOptions()) {
+//			mutator.createMutatedValues(optionName);
+//		}
+		String outputDir = "./sample-config-files-after-mutated";
+		int i = 0;
+		for(MutatedConf mConf : mutatedConfList) {
+			String outputFileName = outputDir + "/" + mutator.parser.getNextMutatedFileName();
+			mConf.writeToFile(outputFileName);
+			i++;
+			if(i > 10) {
+				break;
+			}
+		}
+	}
 }
---
+++ trunk/configuration-detector/main/edu/washington/cs/conf/mutation/MutatedConf.java
@@ -0,0 +1,43 @@
+package edu.washington.cs.conf.mutation;
+
+import java.util.Map;
+
+import edu.washington.cs.conf.util.Files;
+import edu.washington.cs.conf.util.Globals;
+import edu.washington.cs.conf.util.Utils;
+
+public class MutatedConf {
+
+	private final Map<String, String> mutatedConfValues;
+	private final String mutatedConf;
+	private final String originalConfValue;
+	
+	public MutatedConf(Map<String, String> confValues, String mutatedConf, String originalConfValue) {
+		Utils.checkNotNull(confValues);
+		Utils.checkNotNull(mutatedConf);
+		Utils.checkNotNull(originalConfValue);
+		
+		this.mutatedConfValues = confValues;
+		this.mutatedConf = mutatedConf;
+		this.originalConfValue = originalConfValue;
+	}
+	
+	public void writeToFile(String filePath) {
+        StringBuilder sb = new StringBuilder();
+		
+		for(String option : mutatedConfValues.keySet()) {
+			//append comments
+			if(option.equals(mutatedConf)) {
+			    sb.append("##--");
+			    sb.append("original value: " + originalConfValue);
+			    sb.append(Globals.lineSep);
+			}
+			sb.append(option);
+			sb.append("=");
+			sb.append(mutatedConfValues.get(option));
+			sb.append(Globals.lineSep);
+		}
+		
+		Files.writeToFileNoExp(sb.toString(), filePath);
+	}
+}
