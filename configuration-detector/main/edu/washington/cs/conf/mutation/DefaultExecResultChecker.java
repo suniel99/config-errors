ea02276c9456eb730b5d8d5a6f64382c8d83e91a
racezhang <racezhang@gmail.com>
2014-07-08 22:08:57 +0000

    add reflection call

--- configuration-detector/main/edu/washington/cs/conf/mutation/DefaultExecResultChecker.java
+++ configuration-detector/main/edu/washington/cs/conf/mutation/DefaultExecResultChecker.java
@@ -1,5 +1,7 @@
 package edu.washington.cs.conf.mutation;
 
+import java.lang.reflect.Method;
+
 import edu.washington.cs.conf.util.Utils;
 //import junit.framework.TestResult;
 
@@ -11,12 +13,47 @@ public class DefaultExecResultChecker extends ExecResultChecker {
 
 	@Override
 	public boolean pass() {
-		throw new RuntimeException("Not implemented.");
+		if(super.e != null) {
+			return false;
+		}
+		if(super.oracleCheckingMethod == null) {
+			return true;
+		}
+		return this.executeOralceChecking(oracleCheckingMethod);
+	}
+	
+	private boolean executeOralceChecking(String method) {
+		
+		//execute it reflectively
+		int lastIndex = method.lastIndexOf(".");
+		String className = method.substring(0, lastIndex);
+		String methodName = method.substring(lastIndex + 1);
+		
+		boolean result = false;
+		try {
+			Class<?> clz = Class.forName(className);
+			Method m = clz.getDeclaredMethod(methodName);
+			//may need to check the method signature
+			Object object = m.invoke(null);
+			result = (Boolean)object;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		return result;
 	}
 
 	@Override
 	public String fetchMessage() {
-		return null;
+		if(super.e != null) {
+			return super.e.getMessage();
+		}
+		if(pass()) {
+			return null;
+		}
+		String message = MessageAnalyzer.fetchErrorMessage(super.logFile);
+		return message;
 	}
 
 }
--- configuration-detector/main/edu/washington/cs/conf/mutation/ExecResultChecker.java
+++ configuration-detector/main/edu/washington/cs/conf/mutation/ExecResultChecker.java
@@ -1,10 +1,22 @@
 package edu.washington.cs.conf.mutation;
 
+import edu.washington.cs.conf.util.Utils;
+
 public abstract class ExecResultChecker {
 
+	protected Throwable e;
+	protected String logFile;
+	protected String oracleCheckingMethod;
+	
 	//check whether the result is desired or not
 	public ExecResultChecker(Throwable e, String logFile) {
-		
+		this.e = e;
+		this.logFile = logFile;
+	}
+	
+	public void setOracleCheckingMethod(String method) {
+		Utils.checkNotNull(method);
+		this.oracleCheckingMethod = method;
 	}
 	
 	public abstract boolean pass();
