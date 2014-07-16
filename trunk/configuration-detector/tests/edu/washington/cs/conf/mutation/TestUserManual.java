0766602b17c8d0e5e45f5c00e9cb70b5e36f01eb
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2014-07-16 23:00:16 +0000

    update tests

--- trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/TestUserManual.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/TestUserManual.java
@@ -1,12 +1,12 @@
 package edu.washington.cs.conf.mutation;
 
-import edu.washington.cs.conf.mutation.weka.CreateWekaConfig;
+import edu.washington.cs.conf.mutation.weka.TestWekaConfigExamples;
 import junit.framework.TestCase;
 
 public class TestUserManual extends TestCase {
 
 	public void testWekaZeroRManual() {
-		UserManual manual = new UserManual(CreateWekaConfig.zeroR_usermanual);
+		UserManual manual = new UserManual(TestWekaConfigExamples.zeroR_usermanual);
 		for(String key : manual.getAllOptions()) {
 			System.out.println(key + "  =>  " + manual.getDescription(key));
 		}
--- trunk/configuration-detector/tests/edu/washington/cs/conf/nlp/TestIDF.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/nlp/TestIDF.java
@@ -1,7 +1,7 @@
 package edu.washington.cs.conf.nlp;
 
 import edu.washington.cs.conf.mutation.UserManual;
-import edu.washington.cs.conf.mutation.weka.CreateWekaConfig;
+import edu.washington.cs.conf.mutation.weka.TestWekaConfigExamples;
 import edu.washington.cs.conf.util.Utils;
 import junit.framework.TestCase;
 
@@ -9,13 +9,7 @@ public class TestIDF extends TestCase {
 
 	
 	public void testComputeIDF() {
-		UserManual manual = new UserManual(CreateWekaConfig.zeroR_usermanual);
-		for(String key : manual.getAllOptions()) {
-			System.out.println(key + "  =>  " + manual.getDescription(key));
-		}
-		TFIDFWeightCalculator cal = new TFIDFWeightCalculator(manual.getAllTextDesc());
-		cal.computeTFIDFValues();
-		
+		TFIDFWeightCalculator cal = this.createZeroRUserManual();
 //		Utils.sortByValue(map, increase)
 //		for(String w : cal.getAllWords()) {
 //			System.out.println(w + " : " + cal.getIDFValue(w));
@@ -23,4 +17,24 @@ public class TestIDF extends TestCase {
 		cal.printIDFValueDescreasingly();
 	}
 	
+	public void testErrorMessageSimilarity() {
+		TFIDFWeightCalculator cal = this.createZeroRUserManual();
+		BOWAnalyzer analyzer = new BOWAnalyzer(cal);
+		String s1 = "Percentage split ('-percentage-split') is missing.";
+		for(String userManualText : cal.getOriginalText()) {
+			String s2 = userManualText;
+			float similarity = analyzer.computeSimilarity(s1, s2);
+		    System.out.println(s2 + "  : " + similarity);
+		}
+	}
+	
+	private TFIDFWeightCalculator createZeroRUserManual() {
+		UserManual manual = new UserManual(TestWekaConfigExamples.zeroR_usermanual);
+//		for(String key : manual.getAllOptions()) {
+//			System.out.println(key + "  =>  " + manual.getDescription(key));
+//		}
+		TFIDFWeightCalculator cal = new TFIDFWeightCalculator(manual.getAllTextDesc());
+		cal.computeTFIDFValues();
+		return cal;
+	}
 }
