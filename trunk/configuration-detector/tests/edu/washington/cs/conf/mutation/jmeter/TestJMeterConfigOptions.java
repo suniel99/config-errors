8580ee3aa8f99f3e3126b2d955ad328cce954733
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2014-07-21 22:54:02 +0000

    update tests

--- trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/jmeter/TestJMeterConfigOptions.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/jmeter/TestJMeterConfigOptions.java
@@ -1,11 +1,28 @@
 package edu.washington.cs.conf.mutation.jmeter;
 
+import java.util.List;
+
+import edu.washington.cs.conf.mutation.ConfMutator;
+import edu.washington.cs.conf.mutation.MutatedConf;
 import junit.framework.TestCase;
 
 public class TestJMeterConfigOptions extends TestCase {
 
+	static String config_file_1 = "E:\\conf-vul\\programs\\jmeter\\apache-jmeter-2.9\\bin\\jmeter.properties";
+	
+	static String config_file_2 = "E:\\conf-vul\\programs\\jmeter\\apache-jmeter-2.9\\bin\\saveservice.properties";
+	
+	static String config_file_3 = "E:\\conf-vul\\programs\\jmeter\\apache-jmeter-2.9\\bin\\system.properties";
+	
 	public void testSampleConfigFiles() {
-		
+		ConfMutator mutator = new ConfMutator(config_file_1);
+		List<MutatedConf> confs = mutator.mutateConfFile();
+		System.out.println("# of mutated confs: " + confs.size());
+		for(MutatedConf conf : confs) {
+			System.out.println(conf.getMutatedConfOption() + ", " + conf.getMutatedConfValue() + ", orig value: " + conf.getOriginalValue());
+			conf.writeToFile("./tmp-output-folder/tmp.properties");
+			break;
+		}
 	}
 	
 }
--- trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/jmeter/TestJMeterExamples.java
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/jmeter/TestJMeterExamples.java
@@ -5,9 +5,13 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import edu.washington.cs.conf.mutation.ConfMutator;
+import edu.washington.cs.conf.mutation.ExecResult;
+import edu.washington.cs.conf.mutation.ExecResultManager;
 import edu.washington.cs.conf.mutation.MutatedConf;
 import edu.washington.cs.conf.mutation.ProgramRunnerByScript;
 import edu.washington.cs.conf.mutation.ScriptCommand;
@@ -44,8 +48,61 @@ public class TestJMeterExamples extends TestCase {
 		ProgramRunnerByScript runner = new ProgramRunnerByScript();
 		runner.setCommands(cmds);
 		runner.setMutatedConfigs(configs);
+		runner.setOutputFile("./tmp-output-folder/jmeter_output.txt");
 		
-		runner.execute();
+		System.out.println("Num of cmds: " + cmds.size() + ", num of mutated confs: " + configs.size());
+		
+		//set the method checkiing and error msg fetching methods
+		ExecResultManager.setOracleCheckingMethod(JMeterCheckingMethods.jmeterCheckingMethods);
+		ExecResultManager.setMessageFetchingMethod(JMeterCheckingMethods.jmeterMsgFetchingMethods);
+		
+//		ProgramRunnerByScript.VERBOSE = true;
+		
+		Collection<ExecResult> results = runner.execute();
+		
+		for(ExecResult r : results) {
+			System.out.println("Execution result: ");
+			System.out.println(r.getMessage());
+		}
+	}
+	
+	public void testRunMultiConfigsInJMeter() {
+		Collection<ScriptCommand> cmds = new LinkedList<ScriptCommand>();
+		//create a command
+		Collection<String> args = Arrays.asList("-n", "-t", "../threadgroup.jmx", "-l",
+				"../output.jtl", "-j", "../testplan.log");
+//		args = Arrays.asList();
+//		script = "dir";
+		
+		ScriptCommand cmd = new ScriptCommand(dir, script);
+		cmd.addArgs(args);
+		cmds.add(cmd);
+		
+		//set the configs		
+		ConfMutator mutator = new ConfMutator(TestJMeterConfigOptions.config_file_1);
+		Collection<MutatedConf> configs = mutator.mutateConfFile();
+		
+		//create a runner
+		ProgramRunnerByScript runner = new ProgramRunnerByScript();
+		runner.setCommands(cmds);
+		runner.setMutatedConfigs(configs);
+		runner.setConfigFile(TestJMeterConfigOptions.config_file_1);
+		runner.setOutputFile("./tmp-output-folder/jmeter_output.txt");
+		
+		System.out.println("Num of cmds: " + cmds.size() + ", num of mutated confs: " + configs.size());
+		
+		//set the method checkiing and error msg fetching methods
+		ExecResultManager.setOracleCheckingMethod(JMeterCheckingMethods.jmeterCheckingMethods);
+		ExecResultManager.setMessageFetchingMethod(JMeterCheckingMethods.jmeterMsgFetchingMethods);
+		
+		ProgramRunnerByScript.VERBOSE = true;
+		
+		Collection<ExecResult> results = runner.execute();
+		
+		for(ExecResult r : results) {
+			System.out.println("Execution result: ");
+			System.out.println(r.getMessage());
+		}
 	}
 	
 }
