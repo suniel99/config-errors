3031405a6a5a08e34893651c50c6369c1fe25126
racezhang@gmail.com <racezhang@gmail.com@30048e6d-ec34-0e89-0f85-a29fb43b1d7d>
2014-07-21 21:55:38 +0000

    jmeter config example

---
+++ trunk/configuration-detector/tests/edu/washington/cs/conf/mutation/jmeter/TestJMeterConfigOptions.java
@@ -0,0 +1,11 @@
+package edu.washington.cs.conf.mutation.jmeter;
+
+import junit.framework.TestCase;
+
+public class TestJMeterConfigOptions extends TestCase {
+
+	public void testSampleConfigFiles() {
+		
+	}
+	
+}
